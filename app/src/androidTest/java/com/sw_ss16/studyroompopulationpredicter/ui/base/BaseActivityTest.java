1b5bcb609856ee71170b96a39c970d6ce6567452
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2016-03-29 16:35:38 +0200

    [LL,SR] Rename folder

---
+++ app/src/androidTest/java/com/sw_ss16/studyroompopulationpredicter/ui/base/BaseActivityTest.java
@@ -0,0 +1,20 @@
+package com.sw_ss16.studyroompopulationpredicter.ui.base;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lo on 3/29/16.
+ */
+public class BaseActivityTest extends ActivityInstrumentationTestCase2<BaseActivity> {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
