90a2ff34e497466eeac9325ecbf5ac7fbc1c11ce
Sebastian Resch <sebastian.resch@student.tugraz.at>
2016-04-09 00:10:44 +0200

    Add method for fav ids, remove unused stuff

--- app/src/main/java/com/sw_ss16/lc_app/content/LearningCenterContent.java
+++ app/src/main/java/com/sw_ss16/lc_app/content/LearningCenterContent.java
@@ -23,79 +23,12 @@ public class LearningCenterContent {
     // -------------------------------
     // Members
     // -------------------------------
-    /**
-     * Current Map of all
-     */
-    public static final List<LearningCenter> learning_centers_ = new ArrayList<>();
-
-    /**
-     * A map of sample items. Key: sample ID; Value: Item. TODO copypasta
-     */
-    public static final Map<String, LearningCenter> learning_centers_map_ = new HashMap<>(5);
+    private Context app_context; //TODO give this at constructor, so it can be used in methots
 
 
     // -------------------------------
     // Methods
     // -------------------------------
-
-    public static void addLearningCenter(LearningCenter item) {
-        learning_centers_.add(item);
-        learning_centers_map_.put(item.id, item);
-    }
-
-    /**
-     * TODO this should be called on startup of application
-     * @param application_context
-     */
-    public void generateMapsFromDB(Context application_context) {
-        System.out.println("Fill List");
-        Database db = new Database(application_context);
-        SQLiteDatabase sqldb = db.getReadableDatabase();
-
-        String[] columns = new String[]{"ID", "NAME", "DESCRIPTION", "ADDRESS", "IMAGE_IN", "IMAGE_OUT", "CAPACITY"};
-        String[] favcolumns = new String[]{"ID", "IS_FAV"};
-
-
-        Cursor c = sqldb.query("studyrooms", columns, null, null, null, null, null);
-        Cursor isfav = sqldb.query("favstudyrooms", favcolumns, null, null, null, null, null);
-
-        c.moveToFirst();
-        isfav.moveToFirst();
-
-        for (int i = 1; i <= c.getCount(); i++) {
-
-            addLearningCenter(new LearningCenter(c.getString(c.getColumnIndex("ID")),
-                    c.getString(c.getColumnIndex("IMAGE_IN")),
-                    c.getString(c.getColumnIndex("IMAGE_OUT")),
-                    c.getString(c.getColumnIndex("NAME")),
-                    c.getString(c.getColumnIndex("DESCRIPTION")),
-                    c.getString(c.getColumnIndex("ADDRESS")),
-                    (isfav.getInt(isfav.getColumnIndex("IS_FAV")) == 1)));
-
-            // TODO: Remove i == 1
-            if (isfav.getInt(isfav.getColumnIndex("IS_FAV")) == 1 || i == 1) {
-                addLearningCenter(new LearningCenter(c.getString(c.getColumnIndex("ID")),
-                        c.getString(c.getColumnIndex("IMAGE_IN")),
-                        c.getString(c.getColumnIndex("IMAGE_OUT")),
-                        c.getString(c.getColumnIndex("NAME")),
-                        c.getString(c.getColumnIndex("DESCRIPTION")),
-                        c.getString(c.getColumnIndex("ADDRESS")),
-                        (isfav.getInt(isfav.getColumnIndex("IS_FAV")) == 1)));
-            }
-
-            c.moveToNext();
-            isfav.moveToNext();
-
-        }
-
-
-    }
-
-
-    public Map<String, LearningCenter> getMapOfAllLearningCenters(){
-
-        return null;
-    }
 
     public void setLearningCeterToFavorite(int lc_id){
 
@@ -103,6 +36,26 @@ public class LearningCenterContent {
 
     public void setLearningCeterToNoFavorite(int lc_id){
 
+    }
+
+    public List<String> getListOfFavLcIds(Context application_context){
+        ArrayList<String> all_lc_ids = new ArrayList<>();
+
+        Database db = new Database(application_context);
+        SQLiteDatabase sqldb = db.getReadableDatabase();
+        String[] columns = new String[]{"ID"};
+
+        Cursor c = sqldb.query("favstudyrooms", columns, null, null, null, null, null);
+
+        c.moveToFirst();
+
+        for (int i = 1; i <= c.getCount(); i++) {
+
+            all_lc_ids.add(c.getString(c.getColumnIndex("ID")));
+            c.moveToNext();
+        }
+
+        return all_lc_ids;
     }
 
     public List<String> getListOfLcIds(Context application_context){
