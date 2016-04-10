635ed3cc3858ea10c580b4d0b4c53424bf5f8510
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2016-04-10 14:31:05 +0200

    Add test case which opens the settings activity

--- app/src/androidTest/java/com/sw_ss16/lc_app/ui/tests/ListActivityTest.java
+++ app/src/androidTest/java/com/sw_ss16/lc_app/ui/tests/ListActivityTest.java
@@ -6,6 +6,9 @@ import com.robotium.solo.Solo;
 import com.sw_ss16.lc_app.R;
 import com.sw_ss16.lc_app.ui.learning_center_list.ListActivity;
 
+/**
+ * Tests that start from the ListActivity
+ */
 public class ListActivityTest extends ActivityInstrumentationTestCase2<ListActivity> {
 
     private Solo mySolo;
@@ -25,16 +28,16 @@ public class ListActivityTest extends ActivityInstrumentationTestCase2<ListActiv
     }
 
     public void testNavigationDrawerOpen() {
-        mySolo.sleep(1000);
+        mySolo.sleep(500);
         mySolo.clickOnImageButton(0);
-        mySolo.sleep(1000);
+        mySolo.sleep(500);
 
         boolean text_found = mySolo.searchText(getActivity().getString(R.string.navigation_title));
         assertEquals("Required text not found", true, text_found);
     }
 
     public void testStudyRoomDetailOpen() {
-        mySolo.sleep(1000);
+        mySolo.sleep(500);
         // If there are any items in the list
         // The second argument of searchText means it searches only for visible text (not hidden)
         if (!mySolo.searchText(getActivity().getString(R.string.no_fav), true)) {
